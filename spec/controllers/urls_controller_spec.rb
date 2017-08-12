1e091581a6d4dc0a681a257d1a9a0f76f9d6caa8
John Maguire <john.maguire@tenavox.com>
2017-08-12 13:39:36 -0500

    added route for create a new, added test for create route

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -2,7 +2,15 @@ require 'rails_helper'
 
 RSpec.describe UrlsController, type: :controller do
 
-  context 'new' do
+  context 'POST create' do
+    let(:params) {
+      {
+        original: 'https://www.google.com'
+      }
+    }
 
+    it 'creates a new url' do
+      expect{ post :create, params: params }.to change{ Url.count }.by 1
+    end
   end
 end
