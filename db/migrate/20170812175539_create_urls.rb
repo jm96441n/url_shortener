1e0e2470e91e54e4f0c195401dae9c6366cb0975
John Maguire <john.maguire@tenavox.com>
2017-08-12 13:22:42 -0500

    added unique constraint on original url

--- db/migrate/20170812175539_create_urls.rb
+++ db/migrate/20170812175539_create_urls.rb
@@ -1,7 +1,7 @@
 class CreateUrls < ActiveRecord::Migration[5.0]
   def change
     create_table :urls do |t|
-      t.string :original,  null: false
+      t.string :original,  null: false, unique: true
       t.string :shortened, null: false, unique: true
       t.timestamps
     end
