a621525f5189561f4c6f73e2c318240f656cfa5e
John Maguire <john.maguire@tenavox.com>
2017-08-12 19:59:22 -0500

    formatting urls to be in correct format for redirects

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -2,4 +2,13 @@ module UrlsHelper
   def entire_shortened_url url
     "#{request.domain}/#{url.shortened}"
   end
+
+  def format_url url
+    if url[0..3].downcase != 'http'
+      full_url = 'http://' + url
+    else
+      full_url = url
+    end
+    full_url
+  end
 end
